5d9d34aa390b458fb3d847883689436bbe6ce59a
Lee Aplin <lee@substrakt.com>
2022-07-22 09:01:08 +0100

    UPDATE

--- cht.sh
+++ cht.sh
@@ -5,4 +5,16 @@ languages=`printf "python php bash terraform aws vault ansible" | tr ' ' '\n'`
 core_utils=`printf "xargs sed mv awk find" | tr ' ' '\n'`
 
 selected=`printf "${languages}\n${core_utils}" | fzf`
-#selected=``
+read -p "query: " query
+
+# Check if we are selecting a language
+# grep check for a match to see if the selection is in the languages list
+# -q Quiet; Do not return to standard output
+# -s Suppress error messages 
+if printf "$languages" | grep -qs $selected; then
+    # curl the cheat sheet for the selected language plus args
+    curl cht.sh/$selected/`echo $query | tr ' ' '+'`
+else
+    # curl the cheat sheet for the selected coreutil plus args
+    curl cht.sh/$selected~$query
+fi

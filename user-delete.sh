8e29cc0110312b857c5e4d81d9ebbdaadf8de696
Lee Aplin <lee@substrakt.com>
2022-07-24 20:41:34 +0100

    UPDATE: Testing if first user matches deleted user

--- user-delete.sh
+++ user-delete.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Run with curl -s https://raw.githubusercontent.com/discodrive/shell-scripts/main/user-delete.sh
+# Run with heroku apps:table --filter="App Name=american-rep-test" --columns="app name" --no-header | xargs -n 1 heroku run "bash <(curl -s https://raw.githubusercontent.com/discodrive/shell-scripts/main/user-delete.sh)" -a
 
 
 # Email address of the user to delete. Save it to a variable for checks.
 # read -p "Email address of user to delete: " EMAIL
-EMAIL="test@substrakt.com"
+EMAIL="anamaria.brebene@substrakt.com"
 
 # Find the first substrakt user, get the second line (to exclude table headers) and third word (user email)
 USER="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 1p)"

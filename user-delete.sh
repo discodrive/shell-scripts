105d01bd76f02a9fd4b28e7209238bd68fa6916b
Lee Aplin <lee@substrakt.com>
2022-07-24 21:55:10 +0100

    REMOVE: Tests

--- user-delete.sh
+++ user-delete.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-# Run with heroku apps:table --filter="App Name=american-rep-test" --columns="app name" --no-header | xargs -n 1 heroku run "bash <(curl -s https://raw.githubusercontent.com/discodrive/shell-scripts/main/user-delete.sh)" -a
-
+# Run with heroku apps:table --filter="App Name=app-name-heres" --columns="app name" --no-header | xargs -n 1 heroku run "bash <(curl -s https://raw.githubusercontent.com/discodrive/shell-scripts/main/user-delete.sh)" -a
 
 # Email address of the user to delete. Save it to a variable for checks.
-# read -p "Email address of user to delete: " EMAIL
-EMAIL="anamaria.brebene@substrakt.com"
+EMAIL="ian.jones@substrakt.com"
 
 # Find the first substrakt user, get the second line (to exclude table headers) and third word (user email)
 USER="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 1p)"
 USER_ID=$(echo $USER | awk '{print $1}')
 USER_EMAIL=$(echo $USER | awk '{print $2}')
 
-# if the email input matches the queried user, reassign the USER variable to the next valid user
+# If the email input matches the queried user, reassign the USER variable to the next valid user
 # Quiet the output and suppress errors - this is just acting as a conditional check
 if printf $EMAIL | grep -qs $USER_EMAIL; then
     USER_ID="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 2p | awk '{print $1;}')"
 fi
 
-printf $USER_ID
-
-# # Delete the user and reassign
-# ./wp-cli.phar user delete ${EMAIL} --reassign=${USER_ID}
+# Delete the user and reassign
+./wp-cli.phar user delete ${EMAIL} --reassign=${USER_ID}
 

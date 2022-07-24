4cfa8eff7fe04f1bf2d6eba46861d6f33eee9f74
Lee Aplin <lee@substrakt.com>
2022-07-24 20:36:16 +0100

    UPDATE: Testing conditional to check user reassignment

--- user-delete.sh
+++ user-delete.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+# Run with curl -s https://raw.githubusercontent.com/discodrive/shell-scripts/main/user-delete.sh
+
 
 # Email address of the user to delete. Save it to a variable for checks.
 # read -p "Email address of user to delete: " EMAIL
 EMAIL="test@substrakt.com"
 
-# Find the first substrakt user, get the second line (to exclude table headers) and first word (user ID)
-USER="$(./wp-cli.phar user list --fields=ID,display_name,user_email | grep @substrakt | sed -n 2p | awk '{print $1;}')"
-
-
+# Find the first substrakt user, get the second line (to exclude table headers) and third word (user email)
+USER="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 1p)"
+USER_ID=$(echo $USER | awk '{print $1}')
+USER_EMAIL=$(echo $USER | awk '{print $2}')
+]
 # if the email input matches the queried user, reassign the USER variable to the next valid user
 # Quiet the output and suppress errors - this is just acting as a conditional check
-if printf $EMAIL | grep -qs $USER; then
-    USER="$(./wp-cli.phar user list --fields=ID,display_name,user_email | grep @substrakt | sed -n 3p | awk '{print $1;}')"
+if printf $EMAIL | grep -qs $USER_EMAIL; then
+    USER_ID="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 2p | awk '{print $1;}')"
 fi
 
+printf $USER_ID
+
 # # Delete the user and reassign
-# ./wp-cli.phar user delete ${EMAIL} --reassign=${USER}
+# ./wp-cli.phar user delete ${EMAIL} --reassign=${USER_ID}
 

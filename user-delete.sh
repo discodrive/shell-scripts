4a00e9736d101f0b05313eae81b18c2f141aa3ec
Lee Aplin <lee@substrakt.com>
2022-07-24 08:00:37 +0100

    TEST

--- user-delete.sh
+++ user-delete.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Input the email address of the user to delete. Save it to a variable for checks.
-read -p "Email address of user to delete: " EMAIL
+# Email address of the user to delete. Save it to a variable for checks.
+# read -p "Email address of user to delete: " EMAIL
+EMAIL="test@substrakt.com"
 printf $EMAIL
+
 # Find the first substrakt user, get the second line (to exclude table headers) and first word (user ID)
 # USER="$(./wp-cli.phar user list --fields=ID,display_name,user_email | grep @substrakt | sed -n 2p | awk '{print $1;}')"
 
@@ -15,10 +17,4 @@ printf $EMAIL
 
 # # Delete the user and reassign
 # ./wp-cli.phar user delete ${EMAIL} --reassign=${USER}
-
-
-
-
-
-
 

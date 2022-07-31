3c1784ef1d8cdebe297b3533c571d4039521475b
Lee Aplin <lee@substrakt.com>
2022-07-31 20:25:51 +0100

    ADD: User variable

--- user-delete.sh
+++ user-delete.sh
@@ -3,18 +3,19 @@
 
 # Email address of the user to delete. Save it to a variable for checks.
 EMAIL=$1
+USER=$(./wp-cli.phar user list --fields=ID,user_email)
 
 # Check if the specified email address is a user on the site
-if printf $EMAIL | grep -qs "$(./wp-cli.phar user list --fields=ID,user_email)"; then
+if printf $EMAIL | grep -qs "$USER"; then
     # Find the first substrakt user. Set variables for the ID and Email address
-    USER="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 1p)"
+    USER="$($USER | grep @substrakt | sed -n 1p)"
     USER_ID=$(echo $USER | awk '{print $1}')
     USER_EMAIL=$(echo $USER | awk '{print $2}')
 
     # If the email input matches the queried user, reassign the USER_ID variable to the next valid user
     # Quiet the output and suppress errors - this is just acting as a conditional check so we dont want an output
     if printf $EMAIL | grep -qs $USER_EMAIL; then
-        USER_ID="$(./wp-cli.phar user list --fields=ID,user_email | grep @substrakt | sed -n 2p | awk '{print $1;}')"
+        USER_ID="$($USER | grep @substrakt | sed -n 2p | awk '{print $1;}')"
     fi
 
     # Delete the user and reassign

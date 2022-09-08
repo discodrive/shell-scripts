8642a937d19fb5871c42732d13330386e825b08c
Lee Aplin <lee@substrakt.com>
2022-09-08 09:06:20 +0100

    WIP: Invalidation CLI

--- downpour.sh
+++ downpour.sh
@@ -4,8 +4,10 @@
 profiles="$(aws configure list-profiles | fzf)"
 
 # Use the selected profile to list distributions and select with fzf
-distibution="$(aws cloudfront list-distributions --query "DistributionList.Items[].{Id : Id, Name: Origins.Items[0].Id}" --output json --profile $profiles)"
+distribution="$(aws cloudfront list-distributions --query "DistributionList.Items[].{Id : Id, Name: Origins.Items[0].Id}" --output json --profile $profiles)"
 
-name=`printf "$($distribution | jq -c '.[] | .Name')"`
-#id=printf "${distribution}" | jq-c '.[] | .Id'
+name=$(echo $distribution | jq -c '.[] | .Name' | tr -d \" | tr ' ' '\n')
+id=$(echo $distribution | jq -c '.[] | .Id')
+
+echo $name
 

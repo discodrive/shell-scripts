8ff17a18c96acd711ae3a5cc9acd70dfc0732790
Lee Aplin <lee@substrakt.com>
2022-09-10 22:07:46 +0100

    ADD: Gum

--- downpour.sh
+++ downpour.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Select a profile using fzf
-profile="$(aws configure list-profiles | fzf)"
+# Select a profile using gum filter
+profile="$(aws configure list-profiles | gum filter)"
 
-# Use the selected profile to list distributions and select with fzf
-selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Id,Origins.Items[0].Id]' --output text --profile $profile | fzf)"`
-id=`printf "$selected" | awk '{print $1}'`
-read -p "Invalidation path: " path
+# Use the selected profile to list distributions and select with gum filter, prompt for an invalidation path
+selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Origins.Items[0].Id,Id]' --output text --profile $profile | gum filter)"`
+id=`printf "$selected" | awk '{print $2}'`
+path="$(gum input --placeholder 'Enter an invalidation path:')"
 
-aws cloudfront create-invalidation --distribution-id $id --paths "$path" --profile $profile
+# Create the invalidation using the selected ID and the invalidation path
+gum spin --spinner line --title 'Creating the invalidation...' --  aws cloudfront create-invalidation --distribution-id $id --paths $path --profile $profile

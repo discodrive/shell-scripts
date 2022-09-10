66f1de308cf4a1e34d42c4ab06c571cb2913865c
Lee Aplin <lee@substrakt.com>
2022-09-10 08:32:00 +0100

    UPDATE: Invalidation script

--- downpour.sh
+++ downpour.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Select a profile using fzf
-profiles="$(aws configure list-profiles | fzf)"
+profile="$(aws configure list-profiles | fzf)"
 
 # Use the selected profile to list distributions and select with fzf
-selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Id,Origins.Items[0].Id]' --output text --profile $profiles | fzf)"`
+selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Id,Origins.Items[0].Id]' --output text --profile $profile | fzf)"`
 id=`printf "$selected" | awk '{print $1}'`
 read -p "Invalidation path: " path
 

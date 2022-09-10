e3f286ff4cf22b8e4feaf34137dc1cb46ee4987e
Lee Aplin <lee@substrakt.com>
2022-09-10 08:04:27 +0100

    WIP: Invalidation CLI

--- downpour.sh
+++ downpour.sh
@@ -4,13 +4,8 @@
 profiles="$(aws configure list-profiles | fzf)"
 
 # Use the selected profile to list distributions and select with fzf
-distribution="$(aws cloudfront list-distributions --query 'DistributionList.Items[].{Id : Id, Name : Origins.Items[0].Id}' --output json --profile $profiles)"
+selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Id,Origins.Items[0].Id]' --output text --profile $profiles | fzf)"`
+id=`printf "$selected" | awk '{print $1}'`
+read -p "Invalidation path: " path
 
-name=$(echo $distribution | jq -r '.[] | .Name, .Id' | tr -d \" | fzf)
-
-#if printf "$distribution" | grep -qs $name; then
-#    id=$(echo $distribution | jq -c '.[] | .Id')
-#fi
-
-echo $name
-
+aws cloudfront create-invalidation --distribution-id $id --paths "$path" --profile $profile

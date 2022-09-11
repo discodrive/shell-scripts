83d7fc0eb13a3bf050a11e658ff6019718e89865
Lee Aplin <lee@substrakt.com>
2022-09-11 21:52:10 +0100

    UPDATE: Notices and style

--- downpour.sh
+++ downpour.sh
@@ -4,17 +4,25 @@
 profile="$(aws configure list-profiles | gum filter)"
 
 # Use the selected profile to list distributions and select with gum filter, prompt for an invalidation path
-selected=`printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Origins.Items[0].Id,Id]' --output text --profile $profile | gum filter)"`
+selected=`printf "$(gum spin --spinner line --title 'Finding distributions' --show-output -- aws cloudfront list-distributions --query 'DistributionList.Items[*].[Origins.Items[0].Id,Id]' --output text --profile $profile | gum filter)"`
 name=`printf "$selected" | awk '{print $1}'`
 id=`printf "$selected" | awk '{print $2}'`
 path="$(gum input --placeholder 'Enter an invalidation path:')"
 
+message="Invalidation successfully created."
+
 # Create the invalidation using the selected ID and the invalidation path
 gum confirm "Invalidate the CloudFront cache for $name distribution on the following path: $path" && \
-gum spin --spinner line --title 'Creating the invalidation...' --  aws cloudfront create-invalidation --distribution-id $id --paths $path --profile $profile || \
+gum spin --spinner line --title 'Creating the invalidation...' -- aws cloudfront create-invalidation  \
+    --distribution-id $id  \
+    --paths $path \
+    --profile $profile || message="No invalidation created."
+
 gum style \
 	--foreground 212 --border-foreground 212 --border double \
 	--align center --width 50 --margin "1 2" --padding "2 4" \
-	'Invalidation not created.' sleep 2 exit 0
+	$message
 
-gum spin --spinner line --title 'All done!' sleep 2
+sleep 2
+
+

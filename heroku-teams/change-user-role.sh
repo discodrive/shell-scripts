2961af95ff389f6e8aea146df256e4bbafa8c21d
Lee Aplin <lee@substrakt.com>
2022-11-24 11:16:56 +0000

    UPDATE: Roles script

--- heroku-teams/change-user-role.sh
+++ heroku-teams/change-user-role.sh
@@ -3,10 +3,12 @@ TEAMS=$(heroku teams | awk '{print $1;}')
 
 for TEAM in $TEAMS
 do
-    SUBSTRAKT=$(heroku members -t $TEAM | grep @substrakt.co | grep collaborator)
-    EMAIL="$(echo $SUBSTRAKT | awk '{print $1}')"
+    SUBSTRAKT=$(heroku members -t $TEAM | grep @substrakt.co | grep collaborator | awk '{print $1}')
     if [ ! -z "$SUBSTRAKT" ]; then
-        heroku members:add $EMAIL -t $TEAM --role member
+        for MEMBER in $SUBSTRAKT
+        do
+            heroku members:add $MEMBER -t $TEAM --role member
+        done
     else
         echo "No collaborators with @substrakt.* email addresses found in ${TEAM} team."
     fi

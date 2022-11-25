5a06284adec510a21dbafc6c4539cb53b49d1785
Lee Aplin <lee@substrakt.com>
2022-11-25 13:26:58 +0000

    ADD: Check if user is already pending a role change and skip them if true

--- heroku-teams/change-user-role.sh
+++ heroku-teams/change-user-role.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 TEAMS=$(heroku teams | awk '{print $1;}')
 
+function exists_in_list() {
+    LIST=$1
+    DELIMITER=$2
+    VALUE=$3
+    echo $LIST | tr "$DELIMITER" '\n' | grep -F -q -x "$VALUE"
+}
+
 for TEAM in $TEAMS
 do
-    SUBSTRAKT=$(heroku members -t $TEAM | grep @substrakt.co | grep collaborator | awk '{print $1}')
-    if [ ! -z "$SUBSTRAKT" ]; then
-        for MEMBER in $SUBSTRAKT
+    MEMBERS=$(heroku members -t "$TEAM")
+    PENDING=$(echo "$MEMBERS" | grep @substrakt.co | grep pending | awk '{print $1}')
+    COLLABS=$(echo "$MEMBERS" | grep @substrakt.co | grep collaborator | awk '{print $1}')
+
+    if [ ! -z "$COLLABS" ]; then
+        for MEMBER in $COLLABS
         do
-            heroku members:add $MEMBER -t $TEAM --role member
+            if ! exists_in_list "$PENDING" " " $MEMBER; then
+                heroku members:add $MEMBER -t $TEAM --role member
+            else
+                echo "${MEMBER} is already pending a role change in ${TEAM} team."
+            fi
         done
     else
         echo "No collaborators with @substrakt.* email addresses found in ${TEAM} team."

2c60369dde2e3049f43b3e7ec7895f2bef9056ca
Lee Aplin <lee@substrakt.com>
2022-12-02 13:07:56 +0000

    REMOVE: -r

--- cloudfront-simple.sh
+++ cloudfront-simple.sh
@@ -3,5 +3,5 @@
 profile="$(aws configure list-profiles | sort -k 2 | fzf)"
 selected=$(printf "$(aws cloudfront list-distributions --query 'DistributionList.Items[*].[Id,Origins.Items[0].Id]' --output text --profile $profile | fzf)")
 id=$(printf %s "$selected" | awk '{print $1}')
-read -p -r "Invalidation path: " path
+read -p "Invalidation path: " path
 aws cloudfront create-invalidation --distribution-id "$id" --paths "$path" --profile "$profile"

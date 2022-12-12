91a6542026fc621b47965d65c54e17384fed6dfc
Lee Aplin <lee@substrakt.com>
2022-12-12 09:05:07 +0000

    UPDATE: Code snippet

--- update-rds-engine.sh
+++ update-rds-engine.sh
@@ -5,16 +5,19 @@ profiles="$(aws configure list-profiles)"
 for profile in $profiles
 do
     r=$(aws configure get region --profile "$profile")
-    cluster=$(aws rds describe-db-clusters --query "*[].{DBClusterIdentifier:DBClusterIdentifier,EngineVersion:EngineVersion}" --output text --profile "$profile" --region "$r" --no-cli-pager | grep 5.6 | awk '{print $1;}')
+    clusters=$(aws rds describe-db-clusters --query "*[].{DBClusterIdentifier:DBClusterIdentifier,EngineVersion:EngineVersion}" --output text --profile "$profile" --region "$r" --no-cli-pager | grep 5.6 | awk '{print $1;}')
 
-    if [ -n "$cluster" ];then
-        aws rds modify-db-cluster \
-            --db-cluster-identifier "$cluster" \
-            --engine-version "5.7.mysql_aurora.2.11.0" \
-            --preferred-maintenance-window "Wed:03:00-Wed:04:00" \
-            --region "$r" \
-            --profile "$profile" \
-            --no-cli-pager
+    if [ -n "$clusters" ];then
+        for cluster in $clusters
+        do
+            aws rds modify-db-cluster \
+                --db-cluster-identifier "$cluster" \
+                --engine-version "5.7.mysql_aurora.2.11.0" \
+                --preferred-maintenance-window "Wed:03:00-Wed:04:00" \
+                --region "$r" \
+                --profile "$profile" \
+                --no-cli-pager
+        done
     fi
 done
 

530ebe006f73073a7ceb600e0f174b3f405059cc
Lee Aplin <lee.aplin@livebuzz.co.uk>
2024-01-05 12:23:16 +0000

    Update powershell script syntax

--- azure/pipeline-check.ps1
+++ azure/pipeline-check.ps1
@@ -2,36 +2,33 @@
 $pipeline = (az pipelines list --query '[].{Path: path, Name: name, Id: id}' --output table | gum filter)
 
 # Get the path, name and id of the pipeline
-$path = ($pipeline | ForEach-Object { $_.Path })
-$name = ($pipeline | ForEach-Object { $_.Name })
-$id = ($pipeline | ForEach-Object { $_.Id })
+$path = ($pipeline -split '\s+')[0]
+$name = ($pipeline -split '\s+')[1]
+$id = ($pipeline -split '\s+')[2]
 
 $message = "Pipeline started successfully"
 $status = (az pipelines runs list --pipeline-id $id --query "[0].{Name: definition.name, Status: status, Result: result, StartTime: startTime, FinishTime: finishTime}" --output tsv)
 
 # What do you want to do with this pipeline?
 Write-Host "What do you want to do with this pipeline?"
-$option = (gum choose --limit 1 {'Check status','Run the pipeline'})
+$option = (gum choose --limit=1 ('Check status','Run the pipeline'))
 
 switch ($option) {
   "Check status" {
-    gum style `
-      --foreground 212 --border-foreground 212 --border double `
-      --align center --width 50 --margin "1 2" --padding "2 4" `
-      "$status"
+      gum style --foreground 212 --border-foreground 212 --border double --align center --width 75 --margin "1 2" --padding "2 4" "$status"
   }
   "Run the pipeline" {
-    if ((gum confirm "Run the $name Pipeline for $path") -and (gum spin --spinner line --title "Running the $name pipeline..." -- az pipelines run --branch main --id $id)) {
-        $message = "Pipeline started successfully"
-    } else {
-        $message = "Cancelled - Pipeline not run"
-    }
+      gum confirm "Run the $name Pipeline for $path"
 
-    gum style `
-      --foreground 212 --border-foreground 212 --border double `
-      --align center --width 50 --margin "1 2" --padding "2 4" `
-      "$message"
+      if ($? -eq 1) {
+          gum spin --spinner line --title "Running the $name pipeline..." -- az pipelines run --branch main --id $id
+          $message = "Pipeline run successful."
+      } else {
+          $message = "Cancelled - Pipeline not run"
+      }
 
-    Start-Sleep -Seconds 2
+      gum style --foreground 212 --border-foreground 212 --border double --align center --width 50 --margin "1 2" --padding "2 4" "$message"
+
+      Start-Sleep -Seconds 2
   }
 }
